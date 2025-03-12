1dc7d04042889c240c3e97b0ee76ac4488506515
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2025-03-11 23:30:19 -0300

    mejoras en el home

--- Ecommerce/src/app/app.component.ts
+++ Ecommerce/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { LucideAngularModule, User, ShoppingBag, Search, Menu, MapPin, X, MessageSquareMore, Twitter, Instagram, Facebook, MousePointerClick  } from 'lucide-angular'
@@ -11,7 +11,7 @@ import { LucideAngularModule, User, ShoppingBag, Search, Menu, MapPin, X, Messag
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   readonly User = User;
   readonly ShoppingBag = ShoppingBag;
   readonly Search = Search;
@@ -33,4 +33,19 @@ export class AppComponent {
   }
 
   title = 'Ecommerce';
+
+  ngOnInit(): void {
+    window.addEventListener("scroll", () => {
+      const elemento = document.querySelector("header") as HTMLElement | null;
+      if (elemento) {
+        if (window.scrollY > 800) {
+          elemento.classList.add("nav-color");
+        } 
+        else {
+          elemento.classList.remove("nav-color");
+        }
+      }
+    });
+  }
+
 }
